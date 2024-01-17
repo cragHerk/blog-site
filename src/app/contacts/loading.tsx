995b43042255999e5124da0bea31580009860df9
m.backi <108221259+cragHerk@users.noreply.github.com>
2024-01-17 23:37:04 +0100

    Update loading.tsx

--- src/app/contacts/loading.tsx
+++ src/app/contacts/loading.tsx
@@ -3,7 +3,7 @@ import { HashLoader } from "react-spinners";
 export default function Loading() {
   return (
     <div className="bg-gray-300 w-screen h-screen flex items-center justify-center">
-      <HashLoader color="white" size={50} />
+      <HashLoader color="gray" size={50} />
     </div>
   );
 }
