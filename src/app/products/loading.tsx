7dcc4f631d946421afcab317c60365d30e14473a
m.backi <108221259+cragHerk@users.noreply.github.com>
2024-01-18 12:44:25 +0100

    Update loading.tsx

--- src/app/products/loading.tsx
+++ src/app/products/loading.tsx
@@ -3,7 +3,7 @@ import { FadeLoader } from "react-spinners";
 export default function Loading() {
   return (
     <div className="bg-gray-300 w-screen h-screen flex items-center justify-center">
-      <FadeLoader color="white" aria-setsize={50} />
+      <FadeLoader color="gray" aria-setsize={50} />
     </div>
   );
 }
