f7cb70bd5fbbcba83232518de28603b57dbea371
m.backi <108221259+cragHerk@users.noreply.github.com>
2024-01-18 14:07:52 +0100

    9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Head from "next/head";
+
 import Hero from "./components/hero";
 import Section from "./components/section";
 import Clients from "./components/clients";
@@ -7,10 +7,6 @@ import Clients from "./components/clients";
 const HomePage: React.FC = () => {
   return (
     <>
-      <Head>
-        <title>Korepetycje</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Hero />
       <Section />
       <Clients />
